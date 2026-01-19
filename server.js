04f7a7559228fee7a4fa9908e23a7db47f3fc9a9
Shoaib khan <shoaibmangal@yahoo.com>
2026-01-19 23:48:07 +0500

    status and message to x=csrt page api

--- server.js
+++ server.js
@@ -242,6 +242,6 @@ aapp.get('/login', (req, res)=>{
     res.send({status: 1, msg:"login api"})
 })
 aapp.post('/cart', (req, res)=>{
-    res.send()
+    res.send({status:1, msg: 'cart page api'})
 })
 aapp.listen(9000)
