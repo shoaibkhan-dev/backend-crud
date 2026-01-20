8d8132a42b62003969bce87f1bd1bfaf0560c22d
Shoaib khan <shoaibmangal@yahoo.com>
2026-01-20 23:38:51 +0500

    api server

--- server.js
+++ server.js
@@ -242,6 +242,6 @@ aapp.get('/login', (req, res)=>{
     res.send({status: 1, msg:"login api"})
 })
 aapp.post('/cart', (req, res)=>{
-    res.send({status:1, msg: 'cart page api'})
+    res.send({status:1, msg: 'cart page apis'})
 })
 aapp.listen(9000)
